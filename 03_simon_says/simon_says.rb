6d5b11a74242b868cb72b84c42f72a5a96729b73
Arunava <learningdroidarunava@gmail.com>
2017-07-10 20:39:09 +0530

    Fixed simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,24 @@
-#write your code here
+def echo (str)
+	str
+end
+
+def shout (str)
+	str.upcase
+end
+
+def repeat (str, num = 2)
+	return (" #{str}" * num).strip
+end
+
+def start_of_word (str, num = 1)
+	
+	str[0, num]
+end
+
+def first_word (str)
+	str.split[0]
+end
+
+def titleize (str)
+	str.split.map!.with_index{|a, i| i != 0 && (a == 'and' || a == 'the' || a == 'to' || a == 'or' || a == 'is' || a == 'are' || a == 'over') ? a : a.capitalize}.join(' ')
+end
