65c6fdfc05f2f7a0d424f4c3687af50e11843511
Arunava <learningdroidarunava@gmail.com>
2017-07-10 18:59:41 +0530

    Fixed calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
-#write your code here
+def add (a, b)
+	a + b
+end
+
+def subtract (a, b)
+	a - b
+end
+
+def sum (arr)
+	sum = 0
+	
+	for num in arr
+		sum += num
+	end
+	
+	return sum
+end
+
+def multiply (arr)
+	mul = 1
+	
+	for num in arr
+		mul *= num
+	end
+	
+	return mul
+end
+
+def power (a, b)
+	return a**b
+end
+
+def factorial (num)
+	if num == 0 
+		return 1
+	end
+	return num * factorial(num - 1)
+end
