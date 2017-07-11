a25afaad9a75c71604a2f183da5315dcdc06ec6b
Arunava <learningdroidarunava@gmail.com>
2017-07-11 18:42:21 +0530

    Fixed book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+	
+	attr_accessor :name
+
+	def initialize
+		name = "Something"
+	end
+
+	def title=(title)
+		@title = titleize (title)
+	end
+
+	def title
+		@title
+	end
+	
+	def titleize (str)
+		str.split.map!.with_index{|a, i| i != 0 && (a == 'and' || a =='the' || a == 'to' || a == 'or' || a == 'is' || a == 'are' || a == 'in' || a == 'on' || a == 'over' || a == 'under' || a == 'below' || a == 'above' || a == 'of' || a == 'a' || a == 'an') ? a : a.capitalize}.join(' ')
+	end
 end
