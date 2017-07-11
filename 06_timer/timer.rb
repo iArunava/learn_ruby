0d9e15192e97d7df00bf4ed1358d452125cf4d87
Arunava <learningdroidarunava@gmail.com>
2017-07-11 19:20:50 +0530

    Fixed timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
-  #write your code here
+	def initialize
+		@seconds = 0
+		@minutes = 0
+		@hours = 0
+	end
+	
+	def seconds= (seconds)
+		@seconds = seconds%60
+		@minutes = seconds/60 % 60
+		@hours = seconds/3600
+	end
+
+	def seconds
+		@seconds
+	end
+
+	def time_string
+		padded(@hours) + ":" + padded(@minutes) + ":" + padded(@seconds)
+	end
+
+	def padded (num)
+		num.to_s.rjust(2, '0')
+	end
 end
